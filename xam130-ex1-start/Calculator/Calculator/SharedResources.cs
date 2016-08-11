c70f03f14c12d09c3a266b4f63881b170f7d0660
IsYet1 <Don@IsYet.com>
2016-08-11 14:24:36 -0500

    1st part of exercise 3 is working. Consolidated the colors. Added the Namespace.

---
+++ xam130-ex1-start/Calculator/Calculator/SharedResources.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public static class SharedResources
+    {
+        public static Color OpButtonBkColor
+        {
+            get
+            {
+                var color = Color.Navy;
+                color = Color.FromRgb(0xff, 0xa5, 0);
+                return color;
+            }
+        }
+    }
+}
